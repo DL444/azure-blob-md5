9c0c4d2ac17a719d3b28c6969ce8bb906064fbe3
David Lee <DL444@outlook.com>
2021-05-23 00:27:11 +0800

    Add implementation.

--- DL444.AzureBlobMd5/Program.cs
+++ DL444.AzureBlobMd5/Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
 
 namespace DL444.AzureBlobMd5
 {
-    internal class Program
+    internal static class Program
     {
-        private static void Main(string[] args)
+        private static async Task Main(string[] args)
         {
-            
+            (string, Task<string>)[] hashTasks = args.Select(x => (x, GetMd5Base64(x))).ToArray();
+            foreach ((string path, Task<string> task) in hashTasks)
+            {
+                try
+                {
+                    string hash = await task;
+                    Console.WriteLine($"{path}\t{hash}");
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"{path}\tFailed: {ex.GetType()}");
+                }
+            }
+        }
+
+        private static async Task<string> GetMd5Base64(string path)
+        {
+            using FileStream file = File.OpenRead(path);
+            MD5 md5 = MD5.Create();
+            byte[] hash = await md5.ComputeHashAsync(file);
+            return Convert.ToBase64String(hash);
         }
     }
 }
